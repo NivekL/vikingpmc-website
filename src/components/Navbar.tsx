a71400d57ef4087d3e32463b580991e05b92ff9f
NivekL <kevnl_95@hotmail.com>
2023-07-29 16:01:36 +0200

    initial setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src="/assets/Logo.svg" alt="" />
+      </div>
+      <div>
+        <ul>
+          <li>About us</li>
+          <li>Information</li>
+          <li>Viking mod</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
